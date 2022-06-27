ef65096196f534911da96c7ae5a874fa1b12e0d4
MarvelTiter <outlinmt@outlook.com>
2022-06-27 17:23:10 +0800

    IServiceCollection扩展

---
+++ LightORM/Extension/IocExtension.cs
@@ -0,0 +1,21 @@
+﻿using MDbContext.ExpressionSql;
+using MDbContext.ExpressionSql.Interface;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MDbContext
+{
+    public static class IocExtension
+    {
+        public static IServiceCollection UseLightOrm(this IServiceCollection services, Action<ExpressionSqlBuilder> config)
+        {
+            var builder = new ExpressionSqlBuilder();
+            config(builder);
+            var ins = builder.Build();
+            services.AddSingleton(ins);
+            return services;
+        }
+    }
+}
