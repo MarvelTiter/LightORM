49ad33818ac8d1b4015103777f0f0b91fe86e5e3
MarvelTiter <outlinmt@outlook.com>
2022-08-16 14:22:30 +0800

    AdoImpl优化

--- LightORM/ExpressionSql/Ado/AdoImpl.cs
+++ LightORM/ExpressionSql/Ado/AdoImpl.cs
@@ -5,82 +5,88 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Data;
 using System.Threading.Tasks;
-
+using System;
 namespace MDbContext.ExpressionSql.Ado
 {
     public class AdoImpl : IAdo
     {
-        private string currentDb = "MainDb";
+        private string? current = null;
+        private const string MAIN = "MainDb";
         private readonly ConcurrentDictionary<string, DbConnectInfo> dbFactories;
         internal AdoImpl(ConcurrentDictionary<string, DbConnectInfo> dbFactories)
         {
             this.dbFactories = dbFactories;
         }
+
+        IDbConnection CurrentConnection
+        {
+            get
+            {
+                var k = current ?? MAIN;
+                current = null;
+                if (dbFactories.TryGetValue(k, out var conn))
+                {
+                    return conn.CreateConnection();
+                }
+                throw new ArgumentException($"未注册的数据库:{k}");
+            }
+        }
+
         public int Execute(string sql, object param = null)
         {
-            var conn = dbFactories[currentDb].CreateConnection();
-            return conn.Execute(sql, param);
+            return CurrentConnection.Execute(sql, param);
         }
 
         public Task<int> ExecuteAsync(string sql, object param = null)
         {
-            var conn = dbFactories[currentDb].CreateConnection();
-            return conn.ExecuteAsync(sql, param);
+            return CurrentConnection.ExecuteAsync(sql, param);
         }
 
         public DataTable ExecuteDataTable(string sql, object param = null)
         {
-            var conn = dbFactories[currentDb].CreateConnection();
-            return conn.ExecuteTable(sql, param);
+            return CurrentConnection.ExecuteTable(sql, param);
         }
 
         public Task<DataTable> ExecuteDataTableAsync(string sql, object param = null)
         {
-            var conn = dbFactories[currentDb].CreateConnection();
-            return conn.ExecuteTableAsync(sql, param);
+            return CurrentConnection.ExecuteTableAsync(sql, param);
         }
 
         public IEnumerable<T> Query<T>(string sql, object param = null)
         {
-            var conn = dbFactories[currentDb].CreateConnection();
-            return conn.Query<T>(sql, param);
+            return CurrentConnection.Query<T>(sql, param);
         }
 
         public IEnumerable<dynamic> Query(string sql, object param = null)
         {
-            var conn = dbFactories[currentDb].CreateConnection();
-            return conn.Query(sql, param);
+            return CurrentConnection.Query(sql, param);
         }
 
         public Task<List<T>> QueryAsync<T>(string sql, object param = null)
         {
-            var conn = dbFactories[currentDb].CreateConnection();
-            return conn.QueryAsync<T>(sql, param);
+            return CurrentConnection.QueryAsync<T>(sql, param);
         }
 
         public Task<IEnumerable<dynamic>> QueryAsync(string sql, object param = null)
         {
-            var conn = dbFactories[currentDb].CreateConnection();
-            return conn.QueryAsync(sql, param);
+            return CurrentConnection.QueryAsync(sql, param);
         }
-
 
         public T Single<T>(string sql, object param = null)
         {
-            var conn = dbFactories[currentDb].CreateConnection();
-            return conn.QuerySingle<T>(sql, param);
+            return CurrentConnection.QuerySingle<T>(sql, param);
         }
 
         public Task<T> SingleAsync<T>(string sql, object param = null)
         {
-            var conn = dbFactories[currentDb].CreateConnection();
-            return conn.QuerySingleAsync<T>(sql, param);
+            return CurrentConnection.QuerySingleAsync<T>(sql, param);
         }
+
         public IAdo SetDb(string key)
         {
             if (!dbFactories.ContainsKey(key))
-                throw new System.ArgumentException($"未注册的数据库:{key}");
-            currentDb = key;
+                throw new ArgumentException($"未注册的数据库:{key}");
+            current = key;
             return this;
         }
     }
