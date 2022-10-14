6ef15b98b3d9e2e4bd6d60974bda94368983ec03
Marvel YAO <outlinmt@outlook.com>
2022-10-14 16:43:30 +0800

    枚举序列化测试（未完成

--- LightORM.Test2/DemoModel/User.cs
+++ LightORM.Test2/DemoModel/User.cs
@@ -1,7 +1,16 @@
 ﻿using MDbEntity.Attributes;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
 
 namespace LightORM.Test2.DemoModel
 {
+    public enum YesOrNo
+    {
+        [Display(Name = "是")]
+        Yes = 1,
+        [Display(Name = "否")]
+        No = 0,
+    }
     [Table(Name = "USER")]
     public class User
     {
@@ -11,5 +20,9 @@ namespace LightORM.Test2.DemoModel
         public string UserName { get; set; }
         [Column(Name = "PASSWORD")]
         public string Password { get; set; }
+         /// <summary>
+         /// 用户是否启用(1-是，0-否）
+         /// </summary>
+        public YesOrNo Enable { get; set; }
     }
 }
