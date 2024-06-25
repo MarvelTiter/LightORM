702f83325283853a928e05aea0c38c85da5bc6b6
marvel <outlinmt@outlook.com>
2024-06-25 10:49:30 +0800

    修复SqlServer分页查询排序问题

--- LightORM/ExpressionSql/DbHandle/SqlServerDb.cs
+++ LightORM/ExpressionSql/DbHandle/SqlServerDb.cs
@@ -23,16 +23,20 @@ internal class SqlServerDb : IDbHelper
     public void Paging(SelectBuilder builder, StringBuilder sql)
     {
         var orderByString = "";
+        var orderByType = "";
         if (builder.OrderBy.Count == 0)
         {
-            var col = builder.TableInfo.Columns.First(c =>c.IsPrimaryKey);
-            orderByString = $"Sub.{col.ColumnName} ASC";
+            var col = builder.TableInfo.Columns.First(c => c.IsPrimaryKey);
+            orderByString = $"Sub.{col.ColumnName}";
+            orderByType = " ASC";
         }
         else
         {
             orderByString = string.Join(",", builder.OrderBy.Select(s => s.Split('.')[1]));
+            orderByType = (builder.AdditionalValue == null ? "" : $" {builder.AdditionalValue}");
         }
-        sql.Insert(0, $" SELECT ROW_NUMBER() OVER(ORDER BY {orderByString}) ROWNO, Sub.* FROM (\n ");
+        sql.Insert(6, " TOP (100) PERCENT");
+        sql.Insert(0, $" SELECT ROW_NUMBER() OVER(ORDER BY {orderByString}{orderByType}) ROWNO, Sub.* FROM (\n ");
         sql.Append(" \n ) Sub");
         // 子查询筛选 ROWNO
         sql.Insert(0, " SELECT * FROM (\n ");
