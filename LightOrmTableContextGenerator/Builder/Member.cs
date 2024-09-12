ea5f2690d9e5b94181d3b618f29824862268fc71
MarvelTiter_yaoqinglin <outlinmt@outlook.com>
2024-09-12 11:24:20 +0800

    添加子模块，删除文件

--- LightOrmTableContextGenerator/Builder/Member.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace Generators.Shared.Builder;
-
-internal abstract class Node
-{
-    public abstract NodeType Type { get; }
-    public virtual string Indent => "";
-    //public abstract new string ToString();
-}
