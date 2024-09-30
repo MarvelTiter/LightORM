fe0503775a902fe13db743fa710a4b78e65cf0d7
MarvelTiter_yaoqinglin <outlinmt@outlook.com>
2024-09-30 11:08:20 +0800

    fixed push_nuget

--- src/GenShared/Generators.Shared/StringExtensions.cs
+++
@@ -1,9 +0,0 @@
-namespace Generators.Shared;
-
-internal static class StringExtensions
-{
-    public static bool IsNullOrEmpty(this string? value)
-    {
-        return string.IsNullOrEmpty(value);
-    }
-}
