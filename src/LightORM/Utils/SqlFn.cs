ba120ebd18b089079f5c0d223e35798faff19bc4
MarvelTiter_yaoqinglin <outlinmt@outlook.com>
2024-09-30 20:18:38 +0800

    添加窗口函数定义

--- src/LightORM/Utils/SqlFn.cs
+++ src/LightORM/Utils/SqlFn.cs
@@ -5,38 +5,37 @@ using System.Text;
 using System.Threading.Tasks;
 
 #nullable disable
-namespace LightORM
+namespace LightORM;
+
+public class SqlFn
 {
-    public class SqlFn
+    public static int Count(bool exp)
     {
-        public static int Count(bool exp)
-        {
-            return default;
-        }
+        return default;
+    }
 
-        public static int Count()
-        {
-            return default;
-        }
+    public static int Count()
+    {
+        return default;
+    }
 
-        public static T Sum<T>(bool exp, T val)
-        {
-            return default;
-        }
+    public static T Sum<T>(bool exp, T val)
+    {
+        return default;
+    }
 
-        public static T Sum<T>(T val)
-        {
-            return default;
-        }
+    public static T Sum<T>(T val)
+    {
+        return default;
+    }
 
-        public static double Avg<T>(bool exp, T val)
-        {
-            return default;
-        }
+    public static double Avg<T>(bool exp, T val)
+    {
+        return default;
+    }
 
-        public static double Avg<T>(T val)
-        {
-            return default;
-        }
+    public static double Avg<T>(T val)
+    {
+        return default;
     }
 }
---
+++ src/LightORM/Utils/WinFn.cs
@@ -0,0 +1,24 @@
+﻿namespace LightORM;
+
+/// <summary>
+/// 窗口函数/开窗函数
+/// </summary>
+public class WinFn
+{
+    public static IWindowFunction<T> Lag<T>(T? column, int offset = 1, T? defaultValue = default)
+    {
+        return default!;
+    }
+    public static IWindowFunction<int> RowNumber()
+    {
+        return default!;
+    }
+}
+
+
+public interface IWindowFunction<T>
+{
+    IWindowFunction<T> PartitionBy(object? column);
+    IWindowFunction<T> OrderBy(object? column);
+    T Value();
+}
