be1f73aa7bb4df6dbb464630f15833590b659c9c
MarvelTiter_yaoqinglin <outlinmt@outlook.com>
2026-09-17 11:58:49 +0800

    test: 接入 KingbaseES 测试项目
    新增 test/LightORMTest.KingbaseES(net10.0 + MSTest 4.0.2, 引用 KingbaseES provider
    与共享 LightORMTest), 与 Oracle 子项目同构:
    
    - GlobalUsings.cs    : ConnectString 指向 docker 容器 kes(端口 54321)
    - MSTestSettings.cs  : 方法级并行, Workers=1
    - ResultTest/ExecutionTest.cs : 继承共享 ExecutionTest, 覆写 DbType 为
      KingbaseESProvider.KingbaseEs
    
    bin/obj/TestResults 由 .gitignore 覆盖。全量 99 用例 99 通过。

---
+++ test/LightORMTest.KingbaseES/GlobalUsings.cs
@@ -0,0 +1,10 @@
+﻿global using LightORM;
+global using LightORM.Interfaces;
+
+namespace LightORMTest.KingbaseES
+{
+    public static class ConnectString
+    {
+        public static string Value => "Server=localhost; Port=54321; User Id=kingbase; Password=123456; Database=kingbase;";
+    }
+}
---
+++ test/LightORMTest.KingbaseES/ResultTest/ExecutionTest.cs
@@ -0,0 +1,21 @@
+﻿using LightORM;
+using LightORM.Interfaces;
+using LightORM.Providers.KingbaseES.Extensions;
+
+namespace LightORMTest.KingbaseES.ResultTest;
+
+[TestClass]
+public class ExecutionTest : LightORMTest.ResultTest.ExecutionTest
+{
+    public override DbBaseType DbType => LightORM.Providers.KingbaseES.KingbaseESProvider.KingbaseEs;
+
+    protected override void Configura(IExpressionContextSetup option)
+    {
+        option.UseKingbaseES(static option =>
+        {
+            option.MasterConnectionString = ConnectString.Value;
+            
+        });
+        option.UseInterceptor<LightOrmAop>();
+    }
+}
