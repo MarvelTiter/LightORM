1a4008c5c58ee505bd77742870334a5812460e70
MarvelTiter_yaoqinglin <outlinmt@outlook.com>
2026-09-18 11:39:43 +0800

    chore: 移除 test/TestProject1 并同步解决方案
    - 删除 test/TestProject1 下的全部项目源码
    - LightORM.slnx 摘除对应项目引用
    - 注：旧格式 LightORM.sln 仍指向 test\TestProject1\TestProject1.csproj，待后续一并更新

--- test/TestProject1/GlobalUsings.cs
+++
@@ -1,3 +0,0 @@
-global using Microsoft.VisualStudio.TestTools.UnitTesting;
-global using TestProject1.Models;
-global using LightORM;
--- test/TestProject1/TestInitContext.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TestProject1;
-
-public class TestInitContext : DbInitialContext
-{
-    public override void Initialized(IDbInitial db)
-    {
-        db.CreateTable<Product>();
-    }
-}
